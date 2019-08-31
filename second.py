5e414882e2d23586ba31663ae76e593d3cd6f5f4
Ameyg1 <ameyg1@gmail.com>
2019-09-01 02:20:50 +1200

    functions in python

---
+++ second.py
@@ -0,0 +1,18 @@
+#functions and datatypes
+#python automatically assigns a datatype
+f=2
+print(f)
+
+f="Line of code"
+print(f)
+#python intreprets each variable separetely
+
+print("this is a string "+str(123))
+#python cannot print 2 different datatypes
+
+def function():
+    f="function"
+    print(f)
+
+function() #localf
+print(f) #globalf
