5036815a7a711bae1801ea24ac255a60e46acf2e
Ameyg1 <ameyg1@gmail.com>
2019-09-03 17:44:47 +1200

    elif and else statement with ternary operator

--- fourth.py
+++ fourth.py
@@ -1,8 +1,15 @@
 def main():
-    x,y=10,100
+    x,y=100,1000
     if(x<y):
         st="x is less than y"
+    elif(x>y):
+        st="x is greater than y"
+    else:
+        st="x is equal to y"
     print(st)
+    st="x is less than y" if(x<y) else"x is greater than or equal to y" #similar to ternary operator in java
+    print(st)
+
 
 if __name__ == "__main__":
     main()
