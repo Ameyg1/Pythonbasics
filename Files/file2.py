03a7fc2821f46b5bd158c0bdc518593f1d2fa828
Ameyg1 <ameyg1@gmail.com>
2019-09-09 03:44:01 +1200

    os path

---
+++ Files/file2.py
@@ -0,0 +1,15 @@
+import os
+from os import path
+import datetime
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+def main():
+    print(os.name)
+
+
+
+if __name__=="__main__":
+    main()
